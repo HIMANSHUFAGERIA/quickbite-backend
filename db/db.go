5a495bf6f1e57d4973bb47f194e8f43a99aa47dc
HIMANSHUFAGERIA <himanshufageria@gmai.com>
2026-02-23 20:59:02 +0530

    Fix Production Url Issue

--- db/db.go
+++ db/db.go
@@ -14,15 +14,22 @@ import (
 var DB *pgxpool.Pool
 
 func Connect(cfg *config.Config) {
-	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		cfg.DBHost,
-		cfg.DBPort,
-		cfg.DBUser,
-		cfg.DBPassword,
-		cfg.DBName,
-	)
 
+	var connStr string
+
+	if cfg.DatabaseURL != "" {
+		// ✅ Production (Railway)
+		connStr = cfg.DatabaseURL
+	} else {
+		connStr = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			cfg.DBHost,
+			cfg.DBPort,
+			cfg.DBUser,
+			cfg.DBPassword,
+			cfg.DBName,
+		)
+	}
 	pool, err := pgxpool.New(context.Background(), connStr)
 	if err != nil {
 		log.Fatalf("❌ Failed to connect to database: %v", err)
